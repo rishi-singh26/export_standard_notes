e965ec5796eb79b277f39a9f031b23f524b356d5
Rishi Singh <rishisingh0831@gmail.com>
2022-11-19 23:57:30 +0530

    Added choose file button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import { data } from './data';
+import { useState } from "react";
 
 function App() {
+
+	const [data, setData] = useState({ items: [] });
+
+	const handleFileChange = (e) => {
+		const file = e.target.files[0];
+		const reader = new FileReader();
+		reader.readAsText(file);
+		reader.onload = () => {
+			setData(JSON.parse(reader.result));
+		};
+		reader.onerror = () => {
+			alert('File error', reader.error);
+		}
+	}
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 			</header>
-			{data.items.map((item, index) => {
-				return item.content_type === "Note" ? <div className='Note'>
+			<input type={'file'} onChange={handleFileChange}></input>
+			{data.items.length === 0 ? <h1>Select a file</h1> : data.items.map((item, index) => {
+				return item.content_type === "Note" ? <div className='Note' key={index.toString()}>
 					<h3>{item?.content?.title}</h3>
 					<h4>{item?.content?.text}</h4>
 					<button onClick={() => onShare(item?.content?.title, item?.content?.text)}>Share</button>
-				</div> : <div><h2>Not a note</h2></div>
+				</div> : <div key={index.toString()}><h2>Not a note</h2></div>
 			})}
 		</div>
 	);
