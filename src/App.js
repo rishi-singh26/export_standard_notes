f36c5280c6c145145a9a8b5ae2f7602907d27710
Rishi Singh <rishisingh0831@gmail.com>
2022-11-19 23:22:17 +0530

    App ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+			{data.items.map((item, index) => {
+				return item.content_type === "Note" ? <div className='Note'>
+					<h3>{item?.content?.title}</h3>
+					<h4>{item?.content?.text}</h4>
+					<button onClick={() => onShare(item?.content?.title, item?.content?.text)}>Share</button>
+				</div> : <div><h2>Not a note</h2></div>
+			})}
+		</div>
+	);
+}
+
+function onShare(title, text) {
+	const notesExampleText = title + '\n' + text;
+	const shareOptions = {
+		text: notesExampleText,
+		// url: pageUrl
+	};
+
+	navigator.share(shareOptions)
+		.then(() => {
+			console.log('Successful share')
+		})
+		.catch(error => {
+			// Differentiate between user 'AbortError' and internal errors.
+			// E.g. Internal error: could not connect to Web Share interface.
+			if (error.name === 'AbortError') {
+				console.log('Share card was probably just dismissed')
+				// console.log(error.toString())
+				return
+			}
+
+			// Real share error
+			console.warn(error)
+		})
+
 }
 
 export default App;
